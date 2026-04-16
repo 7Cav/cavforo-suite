d93d3bc8c1a36ea44f0efc9b891695a92e2d59ef
syniron <66834451+SyniRon@users.noreply.github.com>
2026-04-16 00:48:22 -0400

    init

---
+++ Setup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Cav7\RosterSearch;
+
+use XF\AddOn\AbstractSetup;
+
+class Setup extends AbstractSetup
+{
+    public function install(array $stepParams = []): void {}
+    public function upgrade(array $stepParams = []): void {}
+    public function uninstall(array $stepParams = []): void {}
+}
